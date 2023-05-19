0590fcd66015db453794255ade988f8d2ebd431e
squall7844 <mizukitsujimoto09@gmail.com>
2023-05-19 23:34:33 +0900

    ♻️ ヘッドライン部分をコンポーネント化

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import Links from '../components/Links'
+import { Headline } from '../components/Headline'
 
 export default function Home() {
   return (
@@ -12,17 +13,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>
-          next.jsを使用してホームページを作成する
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Headline page="index" />
         <Links />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
