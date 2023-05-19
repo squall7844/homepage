a4faef410ea8afe3eee9f4ccc651dc6dece5977a
squall7844 <mizukitsujimoto09@gmail.com>
2023-05-20 00:35:33 +0900

    💫 formaterを使用してコードを綺麗にした

--- components/Headline.js
+++ components/Headline.js
@@ -1,15 +1,14 @@
-import styles from '../styles/Home.module.css'
-
+import styles from "../styles/Home.module.css";
 
 export function Headline(props) {
   return (
     <div>
-        <h1 className={styles.title}>{props.page} Page</h1>
+      <h1 className={styles.title}>{props.page} Page</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/{props.page}.js</code>
-        </p>
+      <p className={styles.description}>
+        Get started by editing{" "}
+        <code className={styles.code}>pages/{props.page}.js</code>
+      </p>
     </div>
   );
 }
