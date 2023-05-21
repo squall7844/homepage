48a812b9e7612e87223d4942f624885155db2af1
squall7844 <mizukitsujimoto09@gmail.com>
2023-05-21 18:55:14 +0900

    ♻️ メイン部分をコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
-import {Links} from "../components/Links";
+import { Links } from "../components/Links";
 import { Headline } from "../components/Headline";
+import { Main } from "../components/Main";
 
 export default function Home() {
   console.log("test");
@@ -12,11 +13,7 @@ export default function Home() {
         <title>About Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <Headline page="about" />
-        <Links />
-      </main>
+      <Main page="About" />
       <Footer />
     </div>
   );
