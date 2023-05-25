11df887a830ec2f15542607e7e9f4ffae6f42705
squall7844 <mizukitsujimoto09@gmail.com>
2023-05-25 22:54:18 +0900

    ♻️ sauce内リファクタリング・相対パスから絶対パスへ変更

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,6 @@
-import styles from "../styles/Home.module.css";
-import { Links } from "../components/Links";
-import { Headline } from "../components/Headline";
+import styles from "src/components/Main/Main.module.css";
+import { Links } from "src/components/Links";
+import { Headline } from "src/components/Headline";
 
 export function Main(props) {
   return (
