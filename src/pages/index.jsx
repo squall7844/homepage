1c531e009987bf2b22d937574947086f1be12573
squall7844 <mizukitsujimoto09@gmail.com>
2023-05-27 08:26:33 +0900

    ✨useEffectｗ導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,16 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
