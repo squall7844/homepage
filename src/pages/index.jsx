5b4b29f6538474200d8ea5fe46f6b3ab2325a2a6
squall7844 <mizukitsujimoto09@gmail.com>
2023-05-28 17:44:31 +0900

    ✨ useStateについて

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,14 +7,26 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = useCallback(
-    () => {
-      if (count < 10) {
-        setCount((count) => count + 1);
-      }
-    },
-    [count]
-  );
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください。");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -29,8 +41,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>カウント</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="Index(ホーム)" />
       <Footer />
     </div>
