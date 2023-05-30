af733b1b11d112f26384bec0a6616de97dd3e9ee
squall7844 <mizukitsujimoto09@gmail.com>
2023-05-31 00:11:54 +0900

    ✨ とりあえず理想の形になった。

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,7 @@ import classes from "src/components/Header//Header.module.css";
 export function Header() {
   return (
     <header className={classes.header}>
+      <div className={classes.outer}></div>
       <div className={classes.inner}>
         <Link className={classes.anchor} href="/">
           Index
@@ -18,6 +19,7 @@ export function Header() {
         <div className={classes.anchor}>a</div>
         <div className={classes.anchor}>a</div>
       </div>
+      <div className={classes.outer}></div>
     </header>
   );
 }
