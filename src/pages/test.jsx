54ae69392b7588af224d3f6f6e302b33e465e589
squall7844 <mizukitsujimoto09@gmail.com>
2023-06-05 22:42:44 +0900

    ✨ 背景画像追加

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,11 +1,16 @@
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import styles from "src/styles/Home.module.css";
+import Image from "next/image";
 
 export default function Test() {
   return (
     <div>
       <Header />
+      <div className={`fixed left-0 top-0 z-[-1] h-screen w-full`}>
+        <Image src={`/speace.jpg`} alt="画像" fill={true}  />
+      </div>
+
       <div className={styles.container}>test</div>
       <div className={styles.container}>test</div>
       <div className={styles.container}>test</div>
