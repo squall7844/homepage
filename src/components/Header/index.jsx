8a8033d83081d767deb39eda964c991593247375
squall7844 <mizukitsujimoto09@gmail.com>
2023-06-05 23:59:02 +0900

    💄 ヘッダーにマップ関数使用

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,46 @@
 import Link from "next/link";
 import classes from "src/components/Header/Header.module.css";
 
+const ITEMS = [
+  {
+    href: "/",
+    title: "Index",
+  },
+  {
+    href: "/about",
+    title: "About",
+  },
+  {
+    href: "/test",
+    title: "Test",
+  },
+  {
+    href: "/",
+    title: "ここに文字を入力",
+  },
+  {
+    href: "/",
+    title: "ここに文字を入力",
+  },
+  {
+    href: "/",
+    title: "ここに文字を入力",
+  },
+];
+
 export function Header() {
   return (
     <header className={classes.header}>
       <div className={classes.outer}></div>
       <div className={classes.inner}>
         <nav>
-          <Link className={classes.text} href="/">
-            Index
-          </Link>
-          <Link className={classes.text} href="/about">
-            About
-          </Link>
-          <Link className={classes.text} href="/test">
-            Test
-          </Link>
-          <Link className={classes.text} href="/">
-            文字をここに入力
-          </Link>
-          <Link className={classes.text} href="/">
-            文字をここに入力
-          </Link>
-          <Link className={classes.text} href="/">
-            文字をここに入力
-          </Link>
+          {ITEMS.map((item) => {
+            return (
+              <Link key={item.href} className={classes.text} href={item.href}>
+                {item.title}
+              </Link>
+            );
+          })}
         </nav>
       </div>
       <div className={classes.outer}></div>
