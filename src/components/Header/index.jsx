46f6ac41077b408b034d35acf44ad5fc2d6362e8
squall7844 <mizukitsujimoto09@gmail.com>
2023-06-07 23:58:22 +0900

    途中まで（ハンバーガーメニュー仕組みはできた）

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import classes from "src/components/Header/Header.module.css";
 
 const ITEMS = [
@@ -29,32 +31,48 @@ const ITEMS = [
 ];
 
 export function Header() {
-  return (
-    <header className={classes.header}>
-      {/* レスポンシブわかるようにするやつ */}
-      <div className="fixed right-0 top-0 m-8 flex h-6 w-6 items-center justify-center rounded-full bg-gray-700 p-3 font-mono text-xs text-white sm:bg-pink-500 md:bg-orange-500 lg:bg-green-500 xl:bg-blue-500">
-        <div className="block  sm:hidden md:hidden lg:hidden xl:hidden">al</div>
-        <div className="hidden sm:block  md:hidden lg:hidden xl:hidden">sm</div>
-        <div className="hidden sm:hidden md:block  lg:hidden xl:hidden">md</div>
-        <div className="hidden sm:hidden md:hidden lg:block  xl:hidden">lg</div>
-        <div className="hidden sm:hidden md:hidden lg:hidden xl:block">xl</div>
-      </div>
-      {/* レスポンシブわかるようにするやつ */}
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuFunction = () => {
+    setOpenMenu(!openMenu);
+  };
 
-      <div className={classes.outer}></div>
-      <div className={classes.inner}>
-        <nav>
-          {ITEMS.map((item) => {
-            return (
-              <Link key={item.href} className={classes.text} href={item.href}>
-                {item.title}
-              </Link>
-            );
-          })}
-        </nav>
-        <div>X</div>
+  return (
+    <div>
+      <header className={classes.header}>
+        <div className={classes.outer}></div>
+        <div className={classes.inner}>
+          <Image src={"/"} alt="画像" width={100} height={100} />
+          {/* ヘッダー */}
+          <nav>
+            {ITEMS.map((item) => {
+              return (
+                <Link key={item.href} className={classes.text} href={item.href}>
+                  {item.title}
+                </Link>
+              );
+            })}
+          </nav>
+          {/* メニューボタン（ハンバーガー） */}
+          <div>
+            <div className={classes.humburger} onClick={() => menuFunction()}>
+              <span className={openMenu ? classes.open : undefined}></span>
+              <span className={openMenu ? classes.open : undefined}></span>
+              <p className={openMenu ? classes.open : undefined}>Menu</p>
+            </div>
+          </div>
+          {/* メニューボタン（ハンバーガー） */}
+        </div>
+        <div className={classes.outer}></div>
+      </header>
+      <div className={`${classes.Menu} ${openMenu ? classes.open : undefined}`}>
+        {ITEMS.map((item) => {
+          return (
+            <Link key={item.href} href={item.href}>
+              {item.title}
+            </Link>
+          );
+        })}
       </div>
-      <div className={classes.outer}></div>
-    </header>
+    </div>
   );
 }
--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -8,9 +8,17 @@ export default function Test() {
     <div>
       <Header />
       <div className={`fixed left-0 top-0 z-[-1] h-screen w-full`}>
-        <Image src={`/speace.jpg`} alt="画像" fill={true}  />
+        {/* <Image src={'/speace.jpg'} alt="画像" fill={true}  /> */}
       </div>
-
+      {/* レスポンシブわかるようにするやつ */}
+      <div className="fixed right-0  m-8 flex h-6 w-6 items-center justify-center rounded-full bg-gray-700 p-3 font-mono text-xs text-white sm:bg-pink-500 md:bg-orange-500 lg:bg-green-500 xl:bg-blue-500">
+        <div className="block  sm:hidden md:hidden lg:hidden xl:hidden">al</div>
+        <div className="hidden sm:block  md:hidden lg:hidden xl:hidden">sm</div>
+        <div className="hidden sm:hidden md:block  lg:hidden xl:hidden">md</div>
+        <div className="hidden sm:hidden md:hidden lg:block  xl:hidden">lg</div>
+        <div className="hidden sm:hidden md:hidden lg:hidden xl:block">xl</div>
+      </div>
+      {/* レスポンシブわかるようにするやつ */}
       <div className={styles.container}>test</div>
       <div className={styles.container}>test</div>
       <div className={styles.container}>test</div>
