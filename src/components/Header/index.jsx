3992ebfaae119ed0abc6289ff5c983e77c15e684
squall7844 <mizukitsujimoto09@gmail.com>
2023-06-11 23:34:46 +0900

    ヘッダー変更

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import classes from "src/components/Header/Header.module.css";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const ITEMS = [
   {
@@ -32,58 +31,71 @@ const ITEMS = [
 
 export function Header() {
   const [openMenu, setOpenMenu] = useState(false);
+  console.log(openMenu);
+
   const menuFunction = () => {
     setOpenMenu(!openMenu);
   };
 
   return (
-    <div>
-      <header className={classes.header}>
-        <div className={classes.inner}>
-          <Image
-            src={"/logo.jpg"}
-            alt="画像"
-            width={100}
-            height={100}
-            className={classes.logo}
-          />
-          {/* ヘッダー */}
-          <nav>
+    <nav className="flex bg-white">
+      {/* ロゴ */}
+      <div className="flex-none  sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1 ">
+        <Image src="/logo.jpg" alt="logo" width={200} height={100} />
+      </div>
+      {/* ロゴ */}
+
+      {/* ハンバーガーメニュー起動時 */}
+      {openMenu ? (
+        <div className="absolute right-0 top-0 z-10 flex min-h-fit  min-w-full flex-row">
+          <div className="basis-1/2"></div>
+
+          <div className="basis-1/2 bg-white">
+            <ul className=" border-l-2 text-center ">
+              <li className="border-b-2 p-2">
+                <button onClick={menuFunction} className="font-bold">
+                  close
+                </button>
+              </li>
+
+              {ITEMS.map((item) => {
+                return (
+                  <li className="border-b-2 p-2 ">
+                    <div onClick={menuFunction}>
+                      <Link key={item.href} href={item.href}>
+                        {item.title}
+                      </Link>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      ) : undefined}
+      {/* ハンバーガーメニュー起動時 */}
+
+      {/* 標準のメニュー */}
+      <div className="m-5 flex-initial font-bold text-[#7d5dcf] ">
+        <ul className="hidden  flex-initial text-left md:flex">
+          <li className="m-5 p-4">
             {ITEMS.map((item) => {
               return (
-                <Link key={item.href} className={classes.text} href={item.href}>
+                <Link key={item.href} href={item.href} className="m-5">
                   {item.title}
                 </Link>
               );
             })}
-          </nav>
-          {/* メニューボタン（ハンバーガー） */}
-          <div>
-            <div className={classes.humburger} onClick={() => menuFunction()}>
-              <span className={openMenu ? classes.open : undefined}></span>
-              <span className={openMenu ? classes.open : undefined}></span>
-              <p className={openMenu ? classes.open : undefined}>Menu</p>
-            </div>
-          </div>
-          {/* メニューボタン（ハンバーガー） */}
-        </div>
-      </header>
-      <div className={`${classes.Menu} ${openMenu ? classes.open : undefined}`}>
-        <div className={classes.close} onClick={() => menuFunction()}>
-          <span></span>
-          <span></span>
-          <p>Close</p>
-        </div>
-        <div>
-          {/* {ITEMS.map((item) => {
-          return (
-            <Link key={item.href} href={item.href}>
-              {item.title}
-            </Link>
-          );
-        })} */}
-        </div>
+          </li>
+        </ul>
       </div>
-    </div>
+      <button
+        onClick={menuFunction}
+        className="absolute right-5 top-5 flex-initial md:hidden"
+      >
+        <Image src="/bars-solid.svg" alt="menu" width={50} height={50} />
+      </button>
+      {/* 標準のメニュー */}
+    </nav>
   );
 }
